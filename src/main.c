663aa07d67b1df432f3ed0e452d7c4c277fe22de
kaizoku-oh <garssallaoui.bayrem@gmail.com>
2020-10-29 19:28:34 +0100

    added cmsis-rtos and downgraded freertos from v10.4.1 to v10.3.1 due to incompatibilities with cmsis-rtos

--- src/main.c
+++ src/main.c
@@ -1,26 +1,29 @@
 #include "main.h"
-#include "FreeRTOS.h"
-#include "task.h"
+#include "cmsis_os.h"
 
-void vTask1(void *pvParameters);
+static void thread_led(void const * argument);
+
+osThreadId stThreadHandle;
 
 int main(void)
 {
   HAL_Init();
 
-  xTaskCreate(vTask1, "Task 1", 1000, NULL, 1 , NULL);
-  vTaskStartScheduler();
+  osThreadDef(thread, thread_led, osPriorityNormal, 0, 128);
+  stThreadHandle = osThreadCreate(osThread(thread), NULL);
+
+  osKernelStart();
   while(1)
   {
   }
 }
 
-void vTask1(void *pvParameters)
+static void thread_led(void const * argument)
 {
   BSP_LED_Init(LED_BLUE);
   while(1)
   {
     BSP_LED_Toggle(LED_BLUE);
-    vTaskDelay(1000);
+    osDelay(1000);
   }
 }
