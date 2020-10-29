a6d24a0b6e49ca5926aabf4535ccc36123df78c8
kaizoku-oh <garssallaoui.bayrem@gmail.com>
2020-10-29 18:49:30 +0100

    implemented freertos as a library

--- src/main.c
+++ src/main.c
@@ -1,18 +1,26 @@
 #include "main.h"
+#include "FreeRTOS.h"
+#include "task.h"
+
+void vTask1(void *pvParameters);
 
 int main(void)
 {
   HAL_Init();
-  BSP_LED_Init(LED_BLUE);
 
+  xTaskCreate(vTask1, "Task 1", 1000, NULL, 1 , NULL);
+  vTaskStartScheduler();
   while(1)
   {
-    BSP_LED_Toggle(LED_BLUE);
-    HAL_Delay(1000);
   }
 }
 
-void SysTick_Handler(void)
+void vTask1(void *pvParameters)
 {
-  HAL_IncTick();
+  BSP_LED_Init(LED_BLUE);
+  while(1)
+  {
+    BSP_LED_Toggle(LED_BLUE);
+    vTaskDelay(1000);
+  }
 }
